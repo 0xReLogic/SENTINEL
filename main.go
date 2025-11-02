47687951f9495aa1fe5731eef56d1a3b4af7fff4
Reza Febryan <maedeeaew@gmail.com>
2025-11-03 07:21:07 +0800

    feat: add version command with build information
    - Add version command to display build info
    - Show version number, commit hash, build date
    - Display Go version and OS/Arch
    - Support ldflags for version injection during build
    - Add SetVersionInfo function for version management

--- main.go
+++ main.go
@@ -4,12 +4,23 @@
 package main
 
 import (
+	"log"
+
 	"github.com/0xReLogic/SENTINEL/cmd"
 	"github.com/joho/godotenv"
-	"log"
+)
+
+// Build information (set via ldflags during build)
+var (
+	version   = "dev"
+	commit    = "none"
+	buildDate = "unknown"
 )
 
 func main() {
+	// Set version info for cmd package
+	cmd.SetVersionInfo(version, commit, buildDate)
+
 	err := godotenv.Load()
 	if err != nil {
 		log.Printf("INFO: Could not load .env file (is that expected?): %v", err)
